1a205fab53d85b84320ec8d2277f25396660c4e9
santietchebarne <santietchebarne@gmail.com>
2023-10-07 21:30:16 -0300

    leer

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tarea;
+use Dotenv\Validator;
 
 class TareaController extends Controller
 {
-    //
-}
+    public function Crear(Request $request){
+        $Tarea=new Tarea;
+        $Validaciones= Validator::make($request->all(),[
+            "titulo"=>"required",
+            "contenido"=>"required",
+            "estado"=>"required",
+            "autor"=>"required",
+        ]);
+        if ($Validaciones->fails()){
+            return response([$Validaciones->errors()], 400);
+        }
+        $Tarea->titulo=$request->titulo;
+        $Tarea->contenido=$request->contenido;
+        $Tarea->estado=$request->estado;
+        $Tarea->autor=$request->autor;
+        $Tarea->save();
+        return $Tarea:
+    }
+    public function Leer($idTarea) {
+        $Tarea=Tarea::find($idTarea);
+        if (!$Tarea) return response(["Mensaje" =>"La tarea no se encuentra"], 400);
+        return $Tarea;
+    }
+
+
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TareaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(TareaController::class)-> group(function (){
+    Route::post("/Tarea", "Crear");
+    Route::get("/Tarea/{id}", "Leer");
+    Route::delete("/Tarea", "Eliminar");
+    Route::put("/Tarea", "Modificar");
 });
